6bb46e77502d0bd00fc2259b553a962d85628683
Dyllan García <dyllangm193@gmail.com>
2024-08-05 22:39:47 -0600

    Mod

--- 01_reflex_agent_mod.js
+++ 01_reflex_agent_mod.js
@@ -1,25 +1,42 @@
-// MIT License
-// Copyright (c) 2020 Luis Espino
+function reflex_agent(location, state) {
+	if (state == "DIRTY") return "SUCK";
+	else if (location == "A") return "RIGHT";
+	else if (location == "B") return "LEFT";
+  }
 
-function reflex_agent(location, state){
-   	if (state=="DIRTY") return "CLEAN";
-   	else if (location=="A") return "RIGHT";
-   	else if (location=="B") return "LEFT";
-}
+  function allStatesVisited(states, visited) {
+	for (let i = 0; i < visited.length; i++) {
+	  if (visited[i][0] === states[0] && visited[i][1] === states[1] && visited[i][2] === states[2]) {
+		return true;
+	  }
+	}
+	return false;
+  }
 
-function test(states){
-      	var location = states[0];		
-      	var state = states[0] == "A" ? states[1] : states[2];
-      	var action_result = reflex_agent(location, state);
-      	document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
-      	if (action_result == "CLEAN"){
-        	if (location == "A") states[1] = "CLEAN";
-         	else if (location == "B") states[2] = "CLEAN";
-      	}
-      	else if (action_result == "RIGHT") states[0] = "B";
-      	else if (action_result == "LEFT") states[0] = "A";		
-	setTimeout(function(){ test(states); }, 2000);
-}
+  function test(states, visited) {
+	if (visited.length >= 8) {
+	  document.getElementById("log").innerHTML += "<br>All states visited!";
+	  return;
+	}
 
-var states = ["A","DIRTY","DIRTY"];
-test(states);
+	var location = states[0];
+	var state = location == "A" ? states[1] : states[2];
+	var action_result = reflex_agent(location, state);
+	document.getElementById("log").innerHTML += "<br>Location: " + location + " | Action: " + action_result;
+
+	if (action_result == "SUCK") {
+	  if (location == "A") states[1] = "CLEAN";
+	  else if (location == "B") states[2] = "CLEAN";
+	} else if (action_result == "RIGHT") states[0] = "B";
+	else if (action_result == "LEFT") states[0] = "A";
+
+	if (!allStatesVisited(states, visited)) {
+	  visited.push([states[0], states[1], states[2]]);
+	}
+
+	setTimeout(function() { test(states, visited); }, 2000);
+  }
+
+  var states = ["A", "DIRTY", "DIRTY"];
+  var visited = [];
+  test(states, visited);
